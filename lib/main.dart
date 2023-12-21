d82bc5e2a4b10614ae59dceb9530cddcaab92549
bisharat62 <bisharatali9111@gmail.com>
2023-12-21 23:07:24 +0500

    kamlesh 11:08 21/12/2023

--- lib/main.dart
+++ lib/main.dart
@@ -31,24 +31,10 @@ void main() async {
     );
     runApp(const MyApp());
   });
-//   await FirebaseMessaging.instance.setAutoInitEnabled(true);
-
-//   FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
-//   await flutterLocalNotificationsPlugin
-//       .resolvePlatformSpecificImplementation<
-//           AndroidFlutterLocalNotificationsPlugin>()
-//       ?.createNotificationChannel(channel);
-
-//   await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
-//     alert: true,
-//     badge: true,
-//     sound: true,
-//   );
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
@@ -67,19 +53,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-// Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
-//   // If you're going to use other Firebase services in the background, such as Firestore,
-//   // make sure you call `initializeApp` before using other Firebase services.
-//   await Firebase.initializeApp();
-//   print('Handling a background message ${message.messageId}');
-// }
-
-// const AndroidNotificationChannel channel = AndroidNotificationChannel(
-//   'high_importance_channel', // id
-//   'High Importance Notifications', // title
-//   // 'This channel is used for important notifications.', // description
-//   importance: Importance.high,
-// );
-// final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-//     FlutterLocalNotificationsPlugin();
